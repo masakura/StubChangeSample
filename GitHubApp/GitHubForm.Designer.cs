7ffed6c4a8f18790bf7b9b52057d528e009e699f
Tomo Masakura <masakura@codearts.co.jp>
2016-04-04 21:49:54 +0900

    first commit.

---
+++ GitHubApp/GitHubForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace GitHubApp
+{
+    partial class GitHubForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.repositoriesDataGrid = new System.Windows.Forms.DataGridView();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FullNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DescriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoriesDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadButton.Location = new System.Drawing.Point(407, 369);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "読み込み";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // repositoriesDataGrid
+            // 
+            this.repositoriesDataGrid.AllowUserToAddRows = false;
+            this.repositoriesDataGrid.AllowUserToDeleteRows = false;
+            this.repositoriesDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.repositoriesDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.repositoriesDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumn,
+            this.FullNameColumn,
+            this.DescriptionColumn});
+            this.repositoriesDataGrid.Location = new System.Drawing.Point(12, 12);
+            this.repositoriesDataGrid.Name = "repositoriesDataGrid";
+            this.repositoriesDataGrid.RowTemplate.Height = 21;
+            this.repositoriesDataGrid.Size = new System.Drawing.Size(470, 351);
+            this.repositoriesDataGrid.TabIndex = 1;
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.DataPropertyName = "name";
+            this.NameColumn.HeaderText = "Name";
+            this.NameColumn.Name = "NameColumn";
+            this.NameColumn.ReadOnly = true;
+            // 
+            // FullNameColumn
+            // 
+            this.FullNameColumn.DataPropertyName = "full_name";
+            this.FullNameColumn.HeaderText = "FullName";
+            this.FullNameColumn.Name = "FullNameColumn";
+            this.FullNameColumn.ReadOnly = true;
+            // 
+            // DescriptionColumn
+            // 
+            this.DescriptionColumn.DataPropertyName = "description";
+            this.DescriptionColumn.HeaderText = "Description";
+            this.DescriptionColumn.Name = "DescriptionColumn";
+            this.DescriptionColumn.ReadOnly = true;
+            // 
+            // GitHubForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 404);
+            this.Controls.Add(this.repositoriesDataGrid);
+            this.Controls.Add(this.loadButton);
+            this.Name = "GitHubForm";
+            this.Text = "GitHubForm";
+            ((System.ComponentModel.ISupportInitialize)(this.repositoriesDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.DataGridView repositoriesDataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DescriptionColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FullNameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+    }
+}
---
+++ GitHubApp/GitHubForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace GitHubApp
+{
+    public partial class GitHubForm : Form
+    {
+        public GitHubForm()
+        {
+            InitializeComponent();
+
+            repositoriesDataGrid.AutoGenerateColumns = false;
+        }
+
+        private async void loadButton_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "My App");
+                var response = await client.GetAsync("https://api.github.com/repositories");
+
+                var text = await response.Content.ReadAsStringAsync();
+                var repositories = JsonConvert.DeserializeObject(text);
+
+/*
+                var repositories = new[]
+                {
+                    new {name = "repo1", full_name = "masakura/repo1", description = "description repo1"},
+                    new {name = "repo2", full_name = "a/longlongreponame", description = "description repo2"},
+                    new {name = "repo3", full_name = "masakura/repo3", description = "description repo3"},
+                    new {name = "repo4", full_name = "masakura/repo4", description = "long long description..."}
+                };
+*/
+
+                repositoriesDataGrid.DataSource = repositories;
+            }
+        }
+    }
+}
